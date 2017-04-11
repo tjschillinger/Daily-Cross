dabfc7bfad6e70c95c52a01325fba7c2d2c4762d
Tim <tjschillinger92>
2017-04-10 18:38:26 -0600

    updated link to act am7 link

--- scripts/missional-reading.js
+++ scripts/missional-reading.js
@@ -4,179 +4,179 @@ if (x==="April 1st"){
 
     document.getElementById("instructions-missional").innerHTML = "Read Judges 6-7";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/JDG.6.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/JDG.6.NIV";
 };
 
 if (x==="April 2nd"){
     document.getElementById("instructions-missional").innerHTML = "Read Judges 8-9";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/JDG.8.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/JDG.8.NIV";
 };
 
 if (x==="April 3nd"){
     document.getElementById("instructions-missional").innerHTML = "Read Judges 10-12";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/JDG.10.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/JDG.10.NIV";
 };
 
 if (x==="April 4th"){
     document.getElementById("instructions-missional").innerHTML = "Read Judges 13-15";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/JDG.13.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/JDG.13.NIV";
 };
 
 if (x==="April 5th"){
     document.getElementById("instructions-missional").innerHTML = "Read Judges 16-18";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/JDG.16.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/JDG.16.NIV";
 };
 
 if (x==="April 6th"){
     document.getElementById("instructions-missional").innerHTML = "Read Judges 19-21";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/JDG.19.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/JDG.19.NIV";
 };
 
 if (x==="April 7th"){
     document.getElementById("instructions-missional").innerHTML = "Read Ruth 1-4";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/RUT.1.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/RUT.1.NIV";
 };
 
 if (x==="April 8th"){
     document.getElementById("instructions-missional").innerHTML = "Read 1 Samuel 1-3";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/1SA.1.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/1SA.1.NIV";
 };
 
 if (x==="April 9th"){
     document.getElementById("instructions-missional").innerHTML = "Read 1 Samuel 4-8";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/1SA.4.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/1SA.4.NIV";
 };
 
 if (x==="April 10th"){
     document.getElementById("instructions-missional").innerHTML = "Read 1 Samuel 9-12";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/1SA.9.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/1SA.9.NIV";
 };
 
 if (x==="April 11th"){
     document.getElementById("instructions-missional").innerHTML = "Read 1 Samuel 13-14";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/1SA.13.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/1SA.13.NIV";
 };
 
 if (x==="April 12th"){
     document.getElementById("instructions-missional").innerHTML = "Read 1 Samuel 15-17";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/1SA.15.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/1SA.15.NIV";
 };
 
 if (x==="April 13th"){
     document.getElementById("instructions-missional").innerHTML = "Read 1 Samuel 18-20";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/1SA.18.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/1SA.18.NIV";
 };
 
 if (x==="April 14th"){
     document.getElementById("instructions-missional").innerHTML = "Read 1 Samuel 21-24";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/1SA.21.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/1SA.21.NIV";
 };
 
 if (x==="April 15th"){
     document.getElementById("instructions-missional").innerHTML = "Read Psalms 7, 27, 31, 34, 52";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/PSA.7.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/PSA.7.NIV";
 };
 
 if (x==="April 16th"){
     document.getElementById("instructions-missional").innerHTML = "Read Psalms 56, 120, 140-42";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/PSA.56.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/PSA.56.NIV";
 };
 
 if (x==="April 17th"){
     document.getElementById("instructions-missional").innerHTML = "Read 1 Samuel 25-27";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/1SA.25.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/1SA.25.NIV";
 };
 
 if (x==="April 18th"){
     document.getElementById("instructions-missional").innerHTML = "Read Psalms 17, 35, 54, 63";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/PSA.17.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/PSA.17.NIV";
 };
 
 if (x==="April 19th"){
     document.getElementById("instructions-missional").innerHTML = "Read 1 Samuel 28-31";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/1SA.28.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/1SA.28.NIV";
 };
 
 if (x==="April 20th"){
     document.getElementById("instructions-missional").innerHTML = "Read Psalms 121, 123-25, 128-30";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/PSA.121.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/PSA.121.NIV";
 };
 
 if (x==="April 21st"){
     document.getElementById("instructions-missional").innerHTML = "Read 2 Samuel 1-4";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/2SA.1.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/2SA.1.NIV";
 };
 
 if (x==="April 22nd"){
     document.getElementById("instructions-missional").innerHTML = "Read Psalms 6, 8-10, 14, 16, 19, 21";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/PSA.6.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/PSA.6.NIV";
 };
 
 if (x==="April 23th"){
     document.getElementById("instructions-missional").innerHTML = "Read 1 Chronicles 1-2";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/1CH.1.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/1CH.1.NIV";
 };
 
 if (x==="April 24th"){
     document.getElementById("instructions-missional").innerHTML = "Read Psalms 43-45, 49, 84-85, 87";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/PSA.43.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/PSA.43.NIV";
 };
 
 if (x==="April 25th"){
     document.getElementById("instructions-missional").innerHTML = "Read 1 Chronicles 3-5";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/1CH.3.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/1CH.3.NIV";
 };
 
 if (x==="April 26th"){
     document.getElementById("instructions-missional").innerHTML = "Read Psalms 73, 77-78";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/PSA.73.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/PSA.73.NIV";
 };
 
 if (x==="April 27th"){
     document.getElementById("instructions-missional").innerHTML = "Read 1 Chronicles 6";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/1CH.6.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/1CH.6.NIV";
 };
 
 if (x==="April 28th"){
     document.getElementById("instructions-missional").innerHTML = "Read Psalms 81, 88, 92-93";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/PSA.81.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/PSA.81.NIV";
 };
 
 if (x==="April 29th"){
     document.getElementById("instructions-missional").innerHTML = "Read 1 Chronicles 7-10";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/1CH.7.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/1CH.7.NIV";
 };
 
 if (x==="April 30th"){
     document.getElementById("instructions-missional").innerHTML = "Read Psalms 102-104";
     
-    document.getElementById("missional-reading").src = "https://www.bible.com/bible/111/PSA.102.NIV";
+    document.getElementById("missional-reading").data = "https://www.bible.com/bible/111/PSA.102.NIV";
 };
--- scripts/practical-reading.js
+++ scripts/practical-reading.js
@@ -4,208 +4,208 @@ if (x==="April 1st"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 1";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.1.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.1.NIV";
 };
 
 if (x==="April 2nd"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 2";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.2.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.2.NIV";
 };
 
 if (x==="April 3rd"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 3";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.3.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.3.NIV";
 };
 
 if (x==="April 4th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 4";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.4.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.4.NIV";
 };
 
 if (x==="April 5th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 5";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.5.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.5.NIV";
 };
 
 if (x==="April 6th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 6";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.6.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.6.NIV";
 };
 
 if (x==="April 7th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 7";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.7.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.7.NIV";
 };
 
 if (x==="April 8th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 8";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.8.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.8.NIV";
 };
 
 if (x==="April 9th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read  Proverbs 9";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.9.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.9.NIV";
 };
 
 if (x==="April 10th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read  Proverbs 10";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.10.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.10.NIV";
 };
 
 if (x==="April 11th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 11";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.11.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.11.NIV";
 };
 
 if (x==="April 12th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 12";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.12.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.12.NIV";
 };
 
 if (x==="April 13th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 13";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.13.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.13.NIV";
 };
 
 if (x==="April 14th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 14";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.14.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.14.NIV";
 };
 
 if (x==="April 15th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 15";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.15.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.15.NIV";
 };
 
 if (x==="April 16th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 16";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.16.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.16.NIV";
 };
 
 if (x==="April 17th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 17";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.17.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.17.NIV";
 };
 
 if (x==="April 18th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 18";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.18.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.18.NIV";
 };
 
 if (x==="April 19th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 19";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.19.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.19.NIV";
 };
 
 if (x==="April 20th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 20";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.20.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.20.NIV";
 };
 
 if (x==="April 21st"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 21";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.21.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.21.NIV";
 };
 
 if (x==="April 22nd"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 22";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.22.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.22.NIV";
 };
 
 if (x==="April 23th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 23";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.23.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.23.NIV";
 };
 
 if (x==="April 24th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 24";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.24.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.24.NIV";
 };
 
 if (x==="April 25th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read  Proverbs 25";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.25.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.25.NIV";
 };
 
 if (x==="April 26st"){
 
     document.getElementById("instructions-practical").innerHTML = "Read  Proverbs 26";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.26.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.26.NIV";
 };
 
 if (x==="April 27th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read  Proverbs 27";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.27.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.27.NIV";
 };
 
 if (x==="April 28th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read  Proverbs 28";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.28.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.28.NIV";
 };
 
 if (x==="April 29th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read  Proverbs 29";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.29.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.29.NIV";
 };
 
 if (x==="April 30th"){
 
     document.getElementById("instructions-practical").innerHTML = "Read Proverbs 30";
     
-    document.getElementById("practical-reading").src = "https://www.bible.com/bible/111/PRO.30.NIV";
+    document.getElementById("practical-reading").data = "https://www.bible.com/bible/111/PRO.30.NIV";
 };
